75f8178f48b4b33d4f87fd274d397c2a0efc4f89
unknown <you@example.com>
2024-07-11 19:56:00 -0300

    add metodos na service de pedido

--- src/main/java/springboot/svendas/controller/PedidoController.java
+++ src/main/java/springboot/svendas/controller/PedidoController.java
@@ -2,10 +2,10 @@ package springboot.svendas.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import springboot.svendas.domain.DTO.PedidoDTO;
+import springboot.svendas.domain.entity.Pedido;
 import springboot.svendas.services.PedidoService;
 
 @RestController
@@ -17,5 +17,13 @@ public class PedidoController {
 
     public PedidoController(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer save(@RequestBody PedidoDTO pedidoDTO) {
+        logger.info("Salvando pedido");
+        Pedido pedido = pedidoService.salvar(pedidoDTO);
+        return pedido.getId();
     }
 }
--- src/main/java/springboot/svendas/services/PedidoService.java
+++ src/main/java/springboot/svendas/services/PedidoService.java
@@ -1,11 +1,21 @@
 package springboot.svendas.services;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import springboot.svendas.domain.DTO.ItemPedidoDTO;
+import springboot.svendas.domain.DTO.PedidoDTO;
+import springboot.svendas.domain.entity.Cliente;
+import springboot.svendas.domain.entity.ItemPedido;
+import springboot.svendas.domain.entity.Pedido;
+import springboot.svendas.domain.entity.Produto;
 import springboot.svendas.domain.repository.ClientesRepository;
 import springboot.svendas.domain.repository.ItensPedidoRepository;
 import springboot.svendas.domain.repository.PedidosRepository;
 import springboot.svendas.domain.repository.ProdutosRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +25,39 @@ public class PedidoService {
     private final ClientesRepository clientesRepository;
     private final ProdutosRepository produtosRepository;
     private final ItensPedidoRepository itensPedidoRepository;
+
+    @Transactional
+    public Pedido salvar(PedidoDTO pedidoDTO) {
+        Integer idCliente = pedidoDTO.getCliente();
+        Cliente cliente = clientesRepository.findById(idCliente).orElse(null);
+
+        Pedido pedido = new Pedido();
+        pedido.setTotal(pedidoDTO.getTotal());
+        pedido.setDataPedido(LocalDate.now());
+        pedido.setCliente(cliente);
+
+        List<ItemPedido> itemsPedido = converterItems(pedido, pedidoDTO.getItens());
+        pedidosRepository.save(pedido);
+        itensPedidoRepository.saveAll(itemsPedido);
+        pedido.setItens(itemsPedido);
+        return pedido;
+    }
+
+    private List<ItemPedido> converterItems(Pedido pedido, List<ItemPedidoDTO> items) {
+        if(items.isEmpty()) {
+            throw new IllegalArgumentException("Pedido não pode ser vazio");
+        }
+
+        return items.stream().map(dto -> {
+            Integer idProduto = dto.getProduto();
+            Produto produto = produtosRepository.findById(idProduto).orElse(null);
+
+            ItemPedido itemPedido = new ItemPedido();
+            itemPedido.setPedido(pedido);
+            itemPedido.setQuantidade(dto.getQuantidade());
+            itemPedido.setProduto(produto);
+            return itemPedido;
+        }).toList();
+    };
 
 }
